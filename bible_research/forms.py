f5dc4ffcb5b5ed9d61a31982052d842b003327c5
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2025-04-24 14:25:31 +0300

    Show example input

--- bible_research/forms.py
+++ bible_research/forms.py
@@ -2,4 +2,8 @@ from django import forms
 
 
 class PassageForm(forms.Form):
-    passage = forms.CharField(label='Enter Bible Passage', max_length=100)
+    passage = forms.CharField(
+        label='Enter Bible Passage',
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'e.g., Job 3 or Job 3:1-7'}),
+    )
