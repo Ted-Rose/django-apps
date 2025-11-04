2a663ad3ae9181af728e62eb1e584d1c165001e2
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2025-11-04 17:06:28 +0200

    Add blog_proxy_template

--- single_pages/views.py
+++ single_pages/views.py
@@ -8,15 +8,20 @@ def twister(request):
 
 
 def spoki_page_view(request):
-    url = "https://spoki.lv/stilsmode/Kas-ar-mani-notika-Cilveki-atceras-savus/932253"
+    url = "https://spoki.lv/joki/-Vecie-joki-vienmer-esot-lieliski/932248"
     try:
-        response = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
         response.raise_for_status()  # Raise an exception for bad status codes
         soup = BeautifulSoup(response.content, 'html.parser')
+        
+        content = str(soup)
 
-        title = soup.find('h1', class_='article-title').get_text() if soup.find('h1', class_='article-title') else "Title not found"
-        content_div = soup.find('div', class_='article-body-content')
-        content = str(content_div) if content_div else "<p>Content not found.</p>"
+        title = soup.find('a', class_='title').get_text(strip=True)
+        # content_div = soup.find('div', class_='title')
+        # content = str(content_div) if content_div else "<p>Content not found.</p>"
 
     except requests.exceptions.RequestException as e:
         title = "Error"
@@ -26,4 +31,4 @@ def spoki_page_view(request):
         'title': title,
         'content': content,
     }
-    return render(request, 'tv_programs/spoki_page.html', context)
+    return render(request, 'blog_proxy_template.html', context)
