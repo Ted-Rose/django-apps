e1c16a2e7ab9ce327d55acc66c09b929ee1678a7
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2025-11-05 09:18:37 +0200

    Pick url example in random

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
+import random
 
 
 def twister(request):
@@ -8,7 +9,18 @@ def twister(request):
 
 
 def spoki_page_view(request):
-    url = "https://spoki.lv/joki/-Vecie-joki-vienmer-esot-lieliski/932248"
+    urls = [
+        "https://spoki.lv/joki/-Vecie-joki-vienmer-esot-lieliski/932248",
+        "https://spoki.lv/foto-izlases/Apkarteja-pasaule-ir-interesanta-/932279",
+        "https://spoki.lv/tribine/Kapec-suni-medz-laizit-kaju-pirkstus/930670",
+        "https://spoki.lv/tribine/Latvijas-hokeja-izlasei-bus-jauni/932287",
+        "https://spoki.lv/tribine/Legendara-restorana-Senite-kupols-vairs/932281",
+        "https://spoki.lv/izgudrojumi/Okeana-ir-atrasti-pieradijumi-par/931284",
+        "https://spoki.lv/tribine/Apstiprinats-Cilveki-patiesam-medz/930619",
+        "https://spoki.lv/tribine/Murkski-tikko-atrisinaja-154-gadus-vecu/929713",
+        "https://spoki.lv/tribine/Noslepums-Vienigie-tris-objekti-kas/928115",
+    ]
+    url = random.choice(urls)
     try:
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
@@ -16,7 +28,7 @@ def spoki_page_view(request):
         response = requests.get(url, headers=headers)
         response.raise_for_status()  # Raise an exception for bad status codes
         soup = BeautifulSoup(response.content, 'html.parser')
-        
+
         content = str(soup)
 
         title = soup.find('a', class_='title').get_text(strip=True)
